e27199fc9ab37841ed20cc33e7a713c32cf87392
bgD91 <32795242+bgD91@users.noreply.github.com>
2020-05-10 12:37:07 +0300

    Added scheduledAt for Event

--- src/typeDefs/query.js
+++ src/typeDefs/query.js
@@ -1,20 +1,20 @@
-const {gql} = require('apollo-server');
+const { gql } = require('apollo-server');
 
 const query = gql`
-  scalar Date
-  scalar Time
-  scalar DateTime
-  
-  type Query {
-    me: User
-    myEvents: [Event]
-  }
-  type Mutation {
-    register(username: String!, password: String!): User
-    login(username: String!, password: String!): User
-    createEvent(title: String!, description: String!): Event
-    deleteEvent(_id: ID!): String!
-  }
+	scalar Date
+	scalar Time
+	scalar DateTime
+
+	type Query {
+		me: User
+		myEvents: [Event]
+	}
+	type Mutation {
+		register(username: String!, password: String!): User
+		login(username: String!, password: String!): User
+		createEvent(title: String!, description: String!, scheduledAt: String): Event
+		deleteEvent(_id: ID!): String!
+	}
 `;
 
 module.exports = {
